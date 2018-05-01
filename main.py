27b22246ec99d49169fd2aa5da840008cd627c9e
FreddeG <fredrik.ano@gmail.com>
2018-05-01 16:02:22 +0200

    Added robert :)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 
+
 class Person:
 	"""docstring for ClassName"""
 	def __init__(self, name):
 		self.name = name
+		self.num = 15
 
 	def printName(self):
 		print(self.name)
@@ -13,12 +15,39 @@ class Person:
 			self.name = input("enter name\n")
 		else:
 			self.name = arg
+		return self.name
+
+class People:
+	def __init__(self):
+		self.dataset = dict()
+
+	def addName(self, name):
+		self.dataset[name] = Person(name)
+	def printPeople(self):
+		#self.dataset["Bob"].name = "Test"
+		new = self.dataset.pop('Bob')
+		temp = new.changeName("Robert")
+		
+		self.dataset[temp] = new
+
+		#self.dataset['Bob'].key() = "Test" 
+		print(self.dataset)
+		#print(self.dataset.pop("Bob"))
+		print([self.dataset[i].num for i in self.dataset.keys()]) #
 
 
+
+Database = People()
+Database.addName("Bob")
+Database.addName("Fredrik")
+Database.addName("Magnus")
+
+Database.printPeople()
 #if __name__ == '__main__':
-person1 = Person("Fredrik")
+#person1 = Person("Fredrik")
 
 
-person1.printName()
+#person1.printName()
 
-person1.changeName()
+#person1.changeName()
+#people = people.
