b0e5f84eb25c9afc2804cc37ac9f262cabef294b
FreddeG <fredrik.ano@gmail.com>
2018-05-01 15:25:02 +0200

    added first struct

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 
 
-print ("hello world")
-print ("test")
-test
- 
+class Person:
+	"""docstring for ClassName"""
+	def __init__(self, name):
+		self.name = name
+
+	def printName(self):
+		print(self.name)
+
+	def changeName(self, arg = ''):
+		if( arg == ''):
+			self.name = input("enter name\n")
+		else:
+			self.name = arg
+
+
+#if __name__ == '__main__':
+person1 = Person("Fredrik")
+
+
+person1.printName()
+
+person1.changeName()
